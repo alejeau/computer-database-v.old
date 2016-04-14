dc90c246fe95c212dd8e51caeecefa4daeb98010
Aurélien LEJEAU <alejeau@excilys.com>
2016-04-14 18:07:24 +0200

    All tests now functionnal

--- src/test/java/com/excilys/formation/computerdb/validators/ComputerValidatorTest.java
+++ src/test/java/com/excilys/formation/computerdb/validators/ComputerValidatorTest.java
@@ -1,15 +1,19 @@
 package com.excilys.formation.computerdb.validators;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.excilys.formation.computerdb.errors.Problem;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "file:src/main/resources/application-context.xml" })
 public class ComputerValidatorTest {
 
 	@Autowired
