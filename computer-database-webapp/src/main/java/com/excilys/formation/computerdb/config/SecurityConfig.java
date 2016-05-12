20f042099fd7e46520c24b4cf8bbcc22224b084c
Aurélien LEJEAU <alejeau@excilys.com>
2016-05-12 09:47:11 +0200

    [MERGE] Update from Gatling.

--- computer-database-webapp/src/main/java/com/excilys/formation/computerdb/config/SecurityConfig.java
+++ computer-database-webapp/src/main/java/com/excilys/formation/computerdb/config/SecurityConfig.java
@@ -1,5 +1,17 @@
 package com.excilys.formation.computerdb.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import com.excilys.formation.computerdb.service.impl.UserDetailsServiceImpl;
 
 @Configuration
